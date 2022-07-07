4bc7b5a6a9e33b3b9da493f1f1eaf507f95e371e
InEmEcI <iikar@bk.ru>
2022-07-07 22:49:04 +0300

    Example005_ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "наташа") {
+   Console.Write("Приет моя любимая, Наташа!!!");
+}
+else {
+  Console.Write("Приет, "); Console.Write(userName);  
+}
